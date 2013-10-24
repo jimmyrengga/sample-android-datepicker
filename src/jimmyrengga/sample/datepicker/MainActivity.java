a3fabdd32eb3d33651480983dbb66560c7cd8011
Jimmy Rengga <j.rengga@gmail.com>
2013-10-24 17:37:14 +0700

    tambah UI + xml sample date

--- src/jimmyrengga/sample/datepicker/MainActivity.java
+++ src/jimmyrengga/sample/datepicker/MainActivity.java
@@ -1,15 +1,27 @@
 package jimmyrengga.sample.datepicker;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.view.Menu;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
+	
+	protected static EditText eText;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		eText = (EditText) findViewById(R.id.editText1);
 	}
 
 	@Override
@@ -18,5 +30,32 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public void changeDate(View view){
+		DialogFragment newFragment = new DatePickerFragment();
+		newFragment.show(getFragmentManager(), "");
+	}
+	
+	private static class DatePickerFragment extends DialogFragment
+    	implements DatePickerDialog.OnDateSetListener {
 
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the current date as the default date in the picker
+		final Calendar c = Calendar.getInstance();
+		int year = c.get(Calendar.YEAR);
+		int month = c.get(Calendar.MONTH);
+		int day = c.get(Calendar.DAY_OF_MONTH);
+		
+		// Create a new instance of DatePickerDialog and return it
+		return new DatePickerDialog(getActivity(), this, year, month, day);
+	}
+	
+	public void onDateSet(DatePicker view, int year, int month, int day) {
+		eText.setText(new StringBuilder().append(month + 1)
+				   .append("-").append(day).append("-").append(year)
+				   .append(" "));
+		}
+	}
+	
 }
